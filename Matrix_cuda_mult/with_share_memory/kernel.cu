9ed2d65f1c7dd160f833720f67725870ef880136
Carlos G. MU <52215770+CarlosGabrielMoralesUmasi@users.noreply.github.com>
2023-06-20 14:02:47 -0500

    Update kernel.cu

--- Matrix_cuda_mult/with_share_memory/kernel.cu
+++ Matrix_cuda_mult/with_share_memory/kernel.cu
@@ -1,7 +1,3 @@
-﻿/*
- * EXAMPLE OF MATRIX-MATRIX MULTIPLICATION: CHAPTER 4
- */
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -22,21 +18,42 @@ using namespace std;
 	} \
 }
 
+#define TILE_WIDTH 4
+
 __global__
 void matrixMulKernel(float* P, float* M, float* N, int Width) {
-    int Col = blockDim.x * blockIdx.x + threadIdx.x;
-    int Row = blockDim.y * blockIdx.y + threadIdx.y;
 
-    // check that only the threads with both Row and Col values are in within range
+    __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
+
+    int tx = threadIdx.x, bx = blockIdx.x;
+    int ty = threadIdx.y, by = blockIdx.y;
+
+    // identify row and column of the d_P element to work on
+    int Row = by * TILE_WIDTH + ty;
+    int Col = bx * TILE_WIDTH + tx;
+
     if (Row < Width && Col < Width) {
+
         float pValue = 0;
-        for (int k = 0; k < Width; k++) {
-            pValue += M[Row * Width + k] * N[k * Width + Col];
+
+        // Loop over the d_M and d_N tiles required to compute the d_P element
+        for (int ph = 0; ph < Width / TILE_WIDTH; ph++) {
+
+            // Collaborative loading of d_M and d_N tiles n to the shared memory
+            Mds[ty][tx] = M[Row * Width + ph * TILE_WIDTH + tx];
+            Nds[ty][tx] = N[(ph * TILE_WIDTH + ty) * Width + Col];
+
+            __syncthreads();
+
+            for (int k = 0; k < TILE_WIDTH; k++) {
+                pValue += Mds[ty][k] * Nds[k][tx];
+            }
+            __syncthreads();
         }
         P[Row * Width + Col] = pValue;
     }
 }
-
 
 void matrixMul(float* h_P, float* h_M, float* h_N, int dim, int numThreads) {
 
@@ -53,11 +70,10 @@ void matrixMul(float* h_P, float* h_M, float* h_N, int dim, int numThreads) {
     // copy h_Pin to device memory
     cudaMemcpy(d_M, h_M, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_N, h_N, size, cudaMemcpyHostToDevice);
-
-    //2. Kernel launch code - with 256 threads per block
-    
+    //int numThreads = 8;
+    //2. Kernel launch code - with TILe_WIDTH^2 threads per block
     dim3 dimGrid(ceil(dim / numThreads), ceil(dim / numThreads), 1);
-    dim3 dimBlock(16, 16, 1);
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1);
     matrixMulKernel << <dimGrid, dimBlock >> > (d_P, d_M, d_N, dim);
 
     //3. copy d_Pout from the device memory
@@ -70,18 +86,19 @@ void matrixMul(float* h_P, float* h_M, float* h_N, int dim, int numThreads) {
 }
 
 int main(int argc, char* argv[]) {
-    int numThreadsList[] = { 32, 64, 128, 256, 512 };
+    int numThreadsList[] = {  32, 64, 128, 256, 512 };
     int numThreadsSize = sizeof(numThreadsList) / sizeof(numThreadsList[0]);
 
-    int dimList[] = { 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 };
+    int dimList[] = { 256, 512, 1024, 2048, 4096, 8192 , 16384, 32768 };
     int dimSize = sizeof(dimList) / sizeof(dimList[0]);
 
     float* h_M, * h_N, * h_P;
+    //int dim = 1240; // assume square matricies
+
     h_M = (float*)malloc(sizeof(float) * dimList[dimSize - 1] * dimList[dimSize - 1]);
     h_N = (float*)malloc(sizeof(float) * dimList[dimSize - 1] * dimList[dimSize - 1]);
     h_P = (float*)malloc(sizeof(float) * dimList[dimSize - 1] * dimList[dimSize - 1]);
 
-    // fill M and N with random float numbers
     srand(time(NULL));
     for (int i = 0; i < dimSize; i++) {
         int dim = dimList[i];
@@ -107,17 +124,13 @@ int main(int argc, char* argv[]) {
 
         cout << "--------------------------" << endl;
     }
-
     // Free host memory
     free(h_M);
     free(h_N);
     free(h_P);
 
-    cout << "¡Multiplicación completada con éxito!" << endl;
+    cout << "ok multiplication completed with success!" << endl;
+
 
     return 0;
 }
-
-
-
-
